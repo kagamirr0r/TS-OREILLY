3ded7d8744310a8fce3cbcb9e1b2d06dde2499a2
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2020-12-07 14:55:10 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+console.log("Hello TypeScript!");
+
+let a = 1 + 2;
+
+let b = a + 1;
+
+let c = {
+  apple: a,
+  banana: b,
+};
+
+let d = c.apple * 4;
+
+// d = null;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
